790893eb0183e97584fa3c67dbd38e01b758af85
Jociro Miguel <jociro.miguel@tistech.co.ao>
2021-08-20 12:32:17 +0100

    Rest, Template

--- src/main/java/com/web/pollsmanagement/service/impl/PollnterfaceImpl.java
+++ src/main/java/com/web/pollsmanagement/service/impl/PollnterfaceImpl.java
@@ -3,7 +3,14 @@ package com.web.pollsmanagement.service.impl;
 import com.web.pollsmanagement.model.Game;
 import com.web.pollsmanagement.service.Pollnterface;
 import com.web.pollsmanagement.service.core.Abstract;
+import org.springframework.stereotype.Service;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "pollService")
+@SessionScoped
+@Service
 public class PollnterfaceImpl extends Abstract implements Pollnterface  {
 
     @Override
